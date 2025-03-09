a3b5a21e7248cbff42c945b7fec6da256f343364
MEKEZ <128120861+mekezmkz@users.noreply.github.com>
2025-03-09 16:24:54 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'MEKEZ'
+description 'Carkey with prop'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
