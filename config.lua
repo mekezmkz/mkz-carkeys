1744b0a1c54e9178f282d2646ee290cb428b9b91
MEKEZ <128120861+mekezmkz@users.noreply.github.com>
2025-03-13 13:26:06 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.OpenKey = 'U' --Key to open/close the vehicle
+
+Config.Notify = {
+    closed = 'Hai chiuso il tuo veicolo', --You closed your vehicle
+    opened = 'Hai aperto il tuo veicolo', --You opened your vehicle
+    errorrr = 'Errore!', --Error!
+    notyour = 'Il veicolo non è tuo', --This vehicle is not yours
+    novehiclenearby = 'Nessun veicolo nelle vicinanze' --No your vehicle nearby
+}
