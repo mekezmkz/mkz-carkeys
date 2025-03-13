81a053702082046aa344300583c320b957086a84
MEKEZ <128120861+mekezmkz@users.noreply.github.com>
2025-03-13 13:25:28 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,12 +3,21 @@ ESX.RegisterServerCallback('clubhouse_script:ControllaChiavi', function(source, 
     if idTarget then
         src = idTarget
     end
-    local xPlayer = ESX.GetPlayerFromId(src)
-    if xPlayer then
-        local found = MySQL.scalar.await('SELECT 1 FROM owned_vehicles WHERE owner = ? AND plate = ?', {xPlayer.identifier, plate})
 
-        cb(found ~= nil)
+    -- Get the player object from the ID
+    local xPlayer = ESX.GetPlayerFromId(src)
+    
+    if xPlayer then
+        -- Using oxmysql for query execution
+        MySQL.scalar('SELECT 1 FROM owned_vehicles WHERE owner = ? AND plate = ?', {xPlayer.identifier, plate}, function(result)
+            if result then
+                cb(true)
+            else
+                cb(false)
+            end
+        end)
     else 
+        -- If xPlayer is nil, return false
         cb(false)
     end
 end)
